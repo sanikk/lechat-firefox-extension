af3320bc3f6298fef3d539c64d3ce5a7a9eb0979
Samuli Nikkilä <sanikk@users.noreply.github.com>
2026-04-26 23:19:45 +0300

    new build system, moved all dist stuff to dist

--- build.js
+++ build.js
@@ -1,9 +1,26 @@
 const esbuild = require('esbuild');
 
-esbuild.build({
-  entryPoints: ['src/main.js'],
-  bundle: true,
-  outfile: 'dist/bundle.js',
-  format: 'iife',
-  globalName: 'MyExtension',
-}).catch(() => process.exit(1));
+Promise.all([
+  esbuild.build({
+    entryPoints: ['src/background_script.js'],
+    bundle: true,
+    minify: true,
+    outfile: 'dist/background_script.js',
+    format: 'iife',
+    globalName: 'LLMNotesBackground',
+  }),
+  esbuild.build({
+    entryPoints: ['src/main.js'],
+    bundle: true,
+    outfile: 'dist/bundle.js',
+    format: 'iife',
+    globalName: 'LLMNotes',
+  }),
+  esbuild.build({
+    entryPoints: ['src/storage_tab.js'],
+    bundle: true,
+    outfile: 'dist/storage_tab.js',
+    format: 'iife',
+    globalName: 'LLMNotesStorage',
+  })
+]).catch(() => process.exit(1));
